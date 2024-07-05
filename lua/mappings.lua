ea7b962aace49850f5d6bffbc0db4324b668fecc
PraditaAdiantama <mpaproject307@gmail.com>
2024-07-05 19:09:07 +0700

    init

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+vim.api.nvim_set_keymap('n', '<C-p>', "<cmd>lua require('telescope.builtin').find_files()<CR>", {noremap = true, silent = true})
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
