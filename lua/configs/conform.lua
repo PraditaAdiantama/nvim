4523504a50bc9df26d681ac347bf9f750b683838
PraditaAdiantama <mpaproject307@gmail.com>
2024-07-29 06:30:17 +0700

    fix: prettier timout

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,22 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-     css = { "prettier" },
-     html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" }
   },
 
-   format_on_save = {
-     -- These options will be passed to conform.format()
-     timeout_ms = 500,
-     lsp_fallback = true,
-   },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+
+  format_after_save = {
+    lsp_format = "fallback",
+    notify_on_error = true
+  }
 }
 
 require("conform").setup(options)
